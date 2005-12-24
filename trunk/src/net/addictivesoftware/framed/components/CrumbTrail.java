1df059008e9248a8632357540a405a07c31253d3
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2005-12-24 23:03:50 +0000

    added crumbtrail component

--- trunk/src/net/addictivesoftware/framed/components/CrumbTrail.java
+++ trunk/src/net/addictivesoftware/framed/components/CrumbTrail.java
@@ -7,17 +7,18 @@ import net.addictivesoftware.framed.Crumb;
 import net.addictivesoftware.framed.pages.Album;
 import net.addictivesoftware.framed.services.FotoPathService;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.tapestry.BaseComponent;
 import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.annotations.InjectObject;
+import org.apache.tapestry.annotations.InjectPage;
 
 public abstract class CrumbTrail extends BaseComponent {
-	private static final Log LOG = LogFactory.getLog(Album.class);
 
 	@InjectObject("service:framed.FotoPathService")
 	public abstract FotoPathService getFotoPathService();
+	
+	@InjectPage("Album")
+	public abstract Album GetAlbum();
 	
 	public ArrayList<Crumb> getCrumbs()  {
 		ArrayList<Crumb> crumbs = new ArrayList<Crumb>();
@@ -25,8 +26,6 @@ public abstract class CrumbTrail extends BaseComponent {
 		StringTokenizer st = new StringTokenizer(currentPath, "/");
 		while (st.hasMoreTokens()) {
 			String crumb = st.nextToken();
-			LOG.debug(currentPath);
-			LOG.debug(crumb);
 			
 			String pathUpto = currentPath.substring(0, currentPath.indexOf(crumb) + crumb.length());
 			crumbs.add(new Crumb(crumb, pathUpto));
@@ -36,6 +35,6 @@ public abstract class CrumbTrail extends BaseComponent {
 	public void goCrumb(IRequestCycle cycle) {
 		Object[] params = cycle.getListenerParameters();
 		getFotoPathService().setCurrentPath((String)params[0]);
-		cycle.activate("Album");
+		cycle.activate(GetAlbum());
 	}
 }
