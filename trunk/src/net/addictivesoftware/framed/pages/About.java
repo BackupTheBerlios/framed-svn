6991ae88a24992ef8e41041120bc7614e714a631
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2005-12-27 14:26:26 +0000

    about page now accessably by anonymnous

--- trunk/src/net/addictivesoftware/framed/pages/About.java
+++ trunk/src/net/addictivesoftware/framed/pages/About.java
@@ -2,7 +2,7 @@ package net.addictivesoftware.framed.pages;
 
 import org.apache.tapestry.annotations.Meta;
 
-@Meta({ "anonymous-access=false", "admin-page=false" })
+@Meta({ "anonymous-access=true", "admin-page=false" })
 public abstract class About extends FramedPage {
 
 }
