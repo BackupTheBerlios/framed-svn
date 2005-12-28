afe9c13115efc84f50895cdc262263b3673d0443
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2005-12-28 12:46:59 +0000

    made Serializable

--- trunk/src/net/addictivesoftware/framed/security/User.java
+++ trunk/src/net/addictivesoftware/framed/security/User.java
@@ -1,8 +1,11 @@
 package net.addictivesoftware.framed.security;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 
-public class User {
+public class User implements Serializable {
+	private static final long serialVersionUID = 3787420128870522366L;
+	
 	private Integer id;
 	private String name;
 	private String email;
