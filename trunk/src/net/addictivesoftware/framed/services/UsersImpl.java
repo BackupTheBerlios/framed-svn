498be469988a5f3cfb6a6e548bab62b2aa057f4e
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2005-12-28 12:51:37 +0000

    changed var names to comply with standards

--- trunk/src/net/addictivesoftware/framed/services/UsersImpl.java
+++ trunk/src/net/addictivesoftware/framed/services/UsersImpl.java
@@ -5,35 +5,35 @@ import java.util.ArrayList;
 import net.addictivesoftware.framed.security.User;
 
 public class UsersImpl implements Users {
-	private ArrayList<User> Users;
+	private ArrayList<User> users;
 
 	public UsersImpl() {
-		Users = new ArrayList<User>();
-		Users.add(new User(1,"Gertjan Assies", "gertjan@assies.info", "test", true));
-		Users.add(new User(2,"Tester", "tester@assies.info", "test", false));
+		users = new ArrayList<User>();
+		users.add(new User(1,"Gertjan Assies", "gertjan@assies.info", "test", true));
+		users.add(new User(2,"Tester", "tester@assies.info", "test", false));
 	}
 	
 	/* (non-Javadoc)
 	 * @see net.addictivesoftware.framed.services.IUsers#getUsers()
 	 */
 	public ArrayList<User> getUsers() {
-		return Users;
+		return users;
 	}
 
 	/* (non-Javadoc)
 	 * @see net.addictivesoftware.framed.services.IUsers#setUsers(java.util.ArrayList)
 	 */
-	public void setUsers(ArrayList<User> users) {
-		Users = users;
+	public void setUsers(ArrayList<User> _users) {
+		users = _users;
 	}
 	
 	/* (non-Javadoc)
 	 * @see net.addictivesoftware.framed.services.IUsers#getUser(java.lang.String)
 	 */
-	public User getUser(String email) {
-		for (int i = 0; i < Users.size(); i++) {
-			User user = Users.get(i);
-			if (email.equals(user.getEmail())) {
+	public User getUser(String _email) {
+		for (int i = 0; i < users.size(); i++) {
+			User user = users.get(i);
+			if (_email.equals(user.getEmail())) {
 				return user;
 			}
 		}
