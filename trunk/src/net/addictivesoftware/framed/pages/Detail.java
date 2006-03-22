c00acebb053d3865cbb9a82572392cf1afc97af5
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2006-03-22 17:05:27 +0000

    anonymous access granted, rights are on a per foto basis now

--- trunk/src/net/addictivesoftware/framed/pages/Detail.java
+++ trunk/src/net/addictivesoftware/framed/pages/Detail.java
@@ -3,7 +3,7 @@ package net.addictivesoftware.framed.pages;
 import org.apache.tapestry.annotations.Meta;
 
 
-@Meta({ "anonymous-access=false", "admin-page=false" })
+@Meta({ "anonymous-access=true", "admin-page=false" })
 public abstract class Detail extends FramedPage {
 	
 }
