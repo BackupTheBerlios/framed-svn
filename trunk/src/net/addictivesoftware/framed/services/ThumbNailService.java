f7553729b6e92eeeb96c84d45dfea0f25987966a
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2006-08-01 21:24:44 +0000

    changed create methods, added some parameters

--- trunk/src/net/addictivesoftware/framed/services/ThumbNailService.java
+++ trunk/src/net/addictivesoftware/framed/services/ThumbNailService.java
@@ -1,19 +1,77 @@
 package net.addictivesoftware.framed.services;
 
+
+/**
+ * @author gertjan
+ *
+ * 
+ */
 public interface ThumbNailService {
 
-	public abstract String create(String _name);
-	public abstract String create(String _name, int _w, int _h);
-	public abstract String create(String _name, int _w, int _h, int _q);
 
+	/**
+	 * @param _basePath
+	 * @param _name
+	 * @return
+	 */
+	public abstract String create(String context, String _basePath, String _name);
+	/**
+	 * @param _basePath
+	 * @param _name
+	 * @param _w
+	 * @param _h
+	 * @return path to thumbnail
+	 */
+	public abstract String create(String context, String _basePath, String _name, int _w, int _h);
+	/**
+	 * @param _basePath
+	 * @param _name
+	 * @param _w
+	 * @param _h
+	 * @param _q
+	 * @return path to thumbnail
+	 */
+	public abstract String create(String context, String _basePath, String _name, int _w, int _h, int _q);
+
+	/**
+	 * @return the quality of the jpg
+	 */
 	public int getQuality() ;
+	/**
+	 * @param quality
+	 */
 	public void setQuality(int quality) ;
 
+	/**
+	 * @return
+	 */
 	public int getThumbHeight() ;
+	/**
+	 * @param thumbHeight
+	 */
 	public void setThumbHeight(int thumbHeight) ;
 	
+	/**
+	 * @return
+	 */
 	public int getThumbWidth() ;
+	/**
+	 * @param thumbWidth
+	 */
 	public void setThumbWidth(int thumbWidth) ;
 
+	/**
+	 * @param imageName
+	 * @return
+	 */
 	public String getThumbName(String imageName) ;
+	
+	/**
+	 * @return
+	 */
+	public String getThumbPath();
+	/**
+	 * @param thumbPath
+	 */
+	public void setThumbPath(String thumbPath);
 }
