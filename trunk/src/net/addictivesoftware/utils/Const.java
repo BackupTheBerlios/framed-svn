369f212e3a2ac3084eca8e6801df78b4a9687f5a
major7 <major7@a477cff6-2903-0410-a78b-c5e4fde3d711>
2006-10-09 21:45:12 +0000

    new static const class to collect certain constant values

---
+++ trunk/src/net/addictivesoftware/utils/Const.java
@@ -0,0 +1,10 @@
+/*
+ * Created on 9-okt-2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package net.addictivesoftware.utils;
+
+public class Const {
+	public static final String SEPARATOR = System.getProperty("file.separator");}
